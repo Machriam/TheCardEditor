8704d064bcd773f777117cb984a6557e2caf845c
Amadeus Lehmann <it@peptides.de>
2023-04-28 13:56:34 +0200

    card set fk should not be nullable

--- TheCardEditor.DataModel/DataModel/Card.cs
+++ TheCardEditor.DataModel/DataModel/Card.cs
@@ -8,7 +8,7 @@ public partial class Card
 
     public long Id { get; set; }
     public string Name { get; set; } = null!;
-    public long? CardSetFk { get; set; }
+    public long CardSetFk { get; set; }
     public string Data { get; set; } = null!;
     public virtual CardSet? CardSetFkNavigation { get; set; }
 }
