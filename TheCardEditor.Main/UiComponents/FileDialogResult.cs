c456c3795096d72ad198bc3b3bb82f1ee46dcfa8
Amadeus Lehmann <it@peptides.de>
2023-06-13 21:46:24 +0200

    first steps with windows file picker

---
+++ TheCardEditor.Main/UiComponents/FileDialogResult.cs
@@ -0,0 +1,7 @@
+﻿namespace TheCardEditor.Main.UiComponents;
+
+public class FileDialogResult
+{
+    public string FilePath { get; set; } = "";
+    public string FileDataBase64 { get; set; } = "";
+}
