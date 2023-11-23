bfe857804729d8d1674cb132e0d53bef7f7b56d1
Amadeus Lehmann <no@mail.com>
2023-11-23 17:50:18 +0100

    main: added migrations

--- TheCardEditor.DataModel/DataModel/DataContext.extensions.cs
+++ TheCardEditor.DataModel/DataModel/DataContext.extensions.cs
@@ -1,28 +1,29 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Collections;
+using System.Globalization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using TheCardEditor.DataModel.Migrations;
 
 namespace TheCardEditor.DataModel.DataModel;
-
-#if DEBUG
-
-public class DataContextDesignFactory : IDesignTimeDbContextFactory<DataContext>
-{
-    public DataContext CreateDbContext(string[] args)
-    {
-        var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
-        optionsBuilder.UseSqlite(@"Data Source=C:\Users\GruselGusel\Desktop\Repos\TheCardEditor\data.sqlite3");
-
-        return new DataContext(optionsBuilder.Options);
-    }
-}
-
-#endif
 
 public partial class DataContext
 {
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
         Database.EnsureCreated();
+    }
+
+    public void Migrate()
+    {
+        var version = Database.SqlQueryRaw<string>("select value from Application where Name='Version'");
+        var resourceSet = DatabaseMigrations.ResourceManager.GetResourceSet(CultureInfo.InvariantCulture, false, false) ??
+            throw new Exception("No Migration Resource found");
+        var currentVersion = "0.0.0";
+        if (version != null) currentVersion = version.FirstOrDefault() ?? currentVersion;
+        foreach (var sqlToApply in IVersionSort.CreateDefault(currentVersion).GetPatchesToApply(resourceSet))
+        {
+            Database.ExecuteSqlRaw(sqlToApply.SQL);
+        }
     }
 
     private partial void OnModelCreatingPartial(ModelBuilder modelBuilder)
---
+++ TheCardEditor.Tests/Migrations/IVersionSortExtensionsTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using System;
+using System.Collections;
+using System.Resources;
+using TheCardEditor.DataModel.Migrations;
+using Xunit;
+
+namespace TheCardEditor.Shared.Tests.Migrations;
+
+public class IVersionSortExtensionsTests
+{
+    private class ResourceReaderMock(IEnumerable<string> values) : IResourceReader
+    {
+        private readonly Dictionary<string, string> _values = values.ToDictionary(v => v);
+
+        public void Close()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public IDictionaryEnumerator GetEnumerator()
+        {
+            return _values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _values.GetEnumerator();
+        }
+    }
+
+    [Theory]
+    [InlineData("1.0.0", 3, "0.0.1", "0.1.0", "1.0.0", "1.0.1", "1.1.0", "1.1.1")]
+    [InlineData("0.2.4", 5, "0.1.0", "0.2.4", "0.3.0", "0.3.5", "1.1.2", "1.2.4", "1.1.100")]
+    public void GetPatchesToApply_ShouldWork(string currentVersion, int expected, params string[] availableVersions)
+    {
+        var expectedVersions = availableVersions.Reverse().Take(expected).Reverse().ToList();
+        Random.Shared.Shuffle(availableVersions);
+        var set = new ResourceSet(new ResourceReaderMock(availableVersions));
+        var result = IVersionSort.CreateDefault(currentVersion).GetPatchesToApply(set);
+        result.Count().Should().Be(expected);
+        result.ToList().Select(r => r.Version.Concat(".")).Should().BeEquivalentTo(expectedVersions);
+    }
+}
