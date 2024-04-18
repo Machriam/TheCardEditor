b82952cb66c72d93eec5d1d384c6c3cce57c85e5
Amadeus Lehmann <no@mail.com>
2024-04-18 16:42:19 +0200

    36-picture-manager-check: For easy finding of not existing pictures and better searching, pictures are now shown inside a grid. Not found pictures are displayed first and in color

--- TheCardEditor.Main/Features/PictureManager/PictureManager.razor.cs
+++ TheCardEditor.Main/Features/PictureManager/PictureManager.razor.cs
@@ -1,67 +1,98 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using TheCardEditor.Main.Core;
+using TheCardEditor.Main.Core.Grid;
 using TheCardEditor.Main.UiComponents;
 using TheCardEditor.Services;
+using TheCardEditor.Shared;
 using TheCardEditor.Shared.DTO;
 
-namespace TheCardEditor.Main.Features.PictureManager
+namespace TheCardEditor.Main.Features.PictureManager;
+
+public class PictureGridModel(long id) : AbstractGridModel(id)
 {
-    public partial class PictureManager
+    [GridMetaData(HeaderName = "Name", Width = 400, Resizable = true)]
+    public string PictureName { get; set; } = "";
+
+    [GridMetaData(HeaderName = "Path", Width = 1200, Resizable = true)]
+    public string Path { get; set; } = "";
+
+    [GridMetaData(HeaderName = "Found")]
+    public string Exists { get; set; } = "";
+}
+
+public partial class PictureManager
+{
+    [Inject] public ServiceAccessor<PictureService> PictureService { get; set; } = default!;
+    [Inject] private IGridViewFactory GridViewFactory { get; set; } = default!;
+    private IGridView _gridView = default!;
+    private const string GridViewId = nameof(PictureManager) + nameof(GridViewId);
+    private List<PictureModel> _pictures = [];
+    private string _selectedPath = "";
+    private Dictionary<long, bool> _existingPictures = [];
+    private long? _selectedPicture;
+
+    public void Initialize()
     {
-        [Inject] public ServiceAccessor<PictureService> PictureService { get; set; } = default!;
-        private List<PictureModel> _pictures = new();
-        private List<string> _relativePaths = new();
-        private string _selectedPath = "";
-        private Dictionary<long, bool> _existingPictures = new();
-        private long? _selectedPicture;
+        _selectedPicture = null;
+        _pictures = [.. PictureService.Execute(ps => ps.GetPictures())];
+        _existingPictures = PictureService.Execute(ps => ps.ValidatePictures()) ?? [];
+        StateHasChanged();
+    }
 
-        public void DeletePicture()
-        {
-            if (_selectedPicture == null) return;
-            PictureService.Execute(ps => ps.DeletePicture(_selectedPicture.Value));
-            Initialize();
-        }
+    public void DeletePicture()
+    {
+        if (_selectedPicture == null) return;
+        PictureService.Execute(ps => ps.DeletePicture(_selectedPicture.Value));
+        Initialize();
+    }
 
-        public string GetBackgroundColor(long id)
-        {
-            if (_selectedPicture == id) return "lightblue";
-            return _existingPictures.TryGetValue(id, out var exists) && exists ? "white" : "yellow";
-        }
+    public string GetBackgroundColor(long id)
+    {
+        if (_selectedPicture == id) return "lightblue";
+        return _existingPictures.TryGetValue(id, out var exists) && exists ? "white" : "yellow";
+    }
 
-        public void OnFolderSelected(FileDialogResult result)
-        {
-            PictureService.Execute(ps => ps.LoadPicturesFromPath(result.FilePath));
-            Initialize();
-        }
+    public void OnFolderSelected(FileDialogResult result)
+    {
+        PictureService.Execute(ps => ps.LoadPicturesFromPath(result.FilePath));
+        Initialize();
+    }
 
-        public void Initialize()
-        {
-            _selectedPicture = null;
-            _pictures = [.. PictureService.Execute(ps => ps.GetPictures())];
-            _existingPictures = PictureService.Execute(ps => ps.ValidatePictures()) ?? new();
-            _relativePaths = _pictures.Select(p => Path.GetDirectoryName(p.Path) ?? "")
-                        .Where(p => !string.IsNullOrEmpty(p))
-                        .Distinct()
-                        .OrderBy(p => p)
-                        .ToList() ?? new();
-            StateHasChanged();
-        }
+    public void PictureSelected(long id)
+    {
+        _selectedPicture = id;
+    }
 
-        public void PictureSelected(long id)
-        {
-            _selectedPicture = id;
-        }
+    public void UpdatePicturePath(FileDialogResult result)
+    {
+        if (_selectedPicture == null) return;
+        PictureService.Execute(ps => ps.UpdatePath(_selectedPicture.Value, result.FilePath));
+        Initialize();
+    }
 
-        public void UpdatePicturePath(FileDialogResult result)
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (!firstRender) return;
+        _gridView = GridViewFactory.CreateGrid(this, GridViewId, OnRowsSelected);
+        var models = _pictures.Select(p => new PictureGridModel(p.Id)
         {
-            if (_selectedPicture == null) return;
-            PictureService.Execute(ps => ps.UpdatePath(_selectedPicture.Value, result.FilePath));
-            Initialize();
-        }
+            PictureName = p.Name,
+            Path = p.Path,
+            Exists = _existingPictures[p.Id] ? "true" : "false",
+            RowColorClass = _existingPictures[p.Id] ? AgGridResources.NoColorRow : AgGridResources.KhakiGridRow
+        }).OrderBy(m => m.Exists)
+        .ThenBy(m => m.Path);
+        await _gridView.UpdateGrid(new DisplayGridModel<PictureGridModel>(models));
+    }
 
-        protected override void OnInitialized()
-        {
-            Initialize();
-            base.OnInitialized();
-        }
+    private void OnRowsSelected(long[] id)
+    {
+    }
+
+    protected override void OnInitialized()
+    {
+        Initialize();
+        base.OnInitialized();
     }
 }
